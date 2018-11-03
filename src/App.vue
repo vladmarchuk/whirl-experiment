2dca4b1d855e184a8571e2a3bd9574623f291271
Vlad Marchuk <vlad.marchuk@haymarket.in.ua>
2018-11-04 00:27:01 +0200

    Initial

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+body,
+html {
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+}
+#nav {
+  padding: 30px;
+  position: absolute;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <RotationBlock></RotationBlock>
+</template>
+
+<script>
+// @ is an alias to /src
+import RotationBlock from "@/components/RotationBlock";
+
+export default {
+  name: "home",
+  components: {
+    RotationBlock
+  }
+};
+</script>
